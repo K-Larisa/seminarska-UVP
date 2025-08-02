18508a7a60ba13924fe1e5ddc57ab2cef8982ba7
K-Larisa <lk62440@student.uni-lj.si>
2025-08-02 18:21:10 +0200

    popolna koda za pridobivanje podatkov

--- izlusci.py
+++ izlusci.py
@@ -12,33 +12,66 @@ import re
 #<span class="subtitle">Giuseppe Rovani</span>
 #<span class="extra">230 downloads</span>
 #</span>
-def izlusci_id_knjige(od,do):
-    
 
+##############################
+#za vsako search stran imamo html datoteko knjige1.html, kjige2.html... s podatki o knjigah
+#odpremo vsak html, ga preberemo ter ven najdemo podatke o id ter naslovu
+#vse skupah shranimo v seznam oblike [(id, naslov), (id, naslov), ...]
+
+def izlusci_id_knjige(od, do):
     #print("Trenutna delovna mapa:", os.getcwd())
+
+    knjige_glavno = []
     for stran in range(od, do):
-        
         with open(os.path.join("knjige", f"knjige{stran}.html"), "r", encoding="utf-8") as dat:
             knjiga = dat.read()
             soup = BeautifulSoup(knjiga, "html.parser")
 
             #za enkrat dobi id in naslov za eno knjigo
             #za vec : 
-            for link in soup.find_all('a', class_='link'):
+            for link in soup.find_all("a", class_="link"):
            #link = soup.find('a', class_='link') #delamo samo z enim linkom
                 if link:
                     # ID iz href-a
-                    href = link.get('href', '')
+                    href = link.get("href", "")
                     #if href.startswith('/ebooks/'): 
                     #    id_knjige = href.split('/ebooks/')[1]
-                    if re.fullmatch(r'/ebooks/\d+', href):   #d+ rabi ker v prvih dveh straneh najde title, ki ni naslov knjige ampak nekaj s search
-                        id_knjige = href.split('/')[2]
+                    if re.fullmatch(r"/ebooks/\d+", href):   #d+ rabi ker v prvih dveh straneh najde title, ki ni naslov knjige ampak nekaj s search
+                        id_knjige = href.split("/")[2]
 
                         # Naslov iz <span class="title">
-                        znacka_title = link.find('span', class_='title')
-                        title = znacka_title.get_text(strip=True) if znacka_title else ''
-
+                        znacka_title = link.find("span", class_="title")
+                        title = znacka_title.get_text(strip=True) if znacka_title else "" #odstrani vodilne in zaključne presledke (strip=True).
+                        
+                        knjige_glavno.append((id_knjige, title))
                         print(f"stran {stran}  ID: {id_knjige}, Naslov: {title}")
+    return(knjige_glavno)
 
 
 
+def izlusci_ostale_podatke(knjige_glavno):
+    for knjiga in knjige_glavno:
+        id = knjiga[0]
+
+        avtor = ""
+        jezik = ""
+        st_prenosov = ""
+        datum_objave = ""
+        zanr = ""
+
+
+        with open(os.path.join("knjige", f"knjige{id}.html"), "r", encoding="utf-8") as dat:
+            podatki = dat.read()
+            soup = BeautifulSoup(podatki, "html.parser")
+
+        avtor = knjiga.find("span", class_="subtitle")
+        avtor = avtor.get_text(strip=True) if avtor else ""
+
+
+
+
+
+
+
+#.text.strip() in .get_text(strip=True) za enostavne <span> deluje enako, za gnezdene elemente <span><b>Ime</b> <i>Avtor</i></span>
+#pa je bolje .get..., saj zdruzi vse podelemente, pocisti presledke
--- pridobi.py
+++ pridobi.py
@@ -7,7 +7,7 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
 }
 
-def shrani_podatke_o_knjigah(od,do):
+def shrani_podatke_o_knjigah(od, do):
     os.makedirs("knjige", exist_ok=True)
     for stran in range(od, do):
         odgovor = requests.get(f"https://www.gutenberg.org/ebooks/search/?sort_order=downloads&start_index={(stran - 1) * 25 + 1}",
@@ -23,3 +23,15 @@ def shrani_podatke_o_knjigah(od,do):
         time.sleep(1)  #da ne obremeni streznika
     print("konec shrani_podatke_o_knjigah")
 
+
+def shrani_html_o_posamezni_knjigi(knjige_glavno): #pobira ven iz seznama [(id, naslov), (id, naslov)...]
+    for knjiga in knjige_glavno:
+        id = knjiga[0]
+        odgovor = requests.get(f"https://www.gutenberg.org/ebooks/{id}", headers=headers)
+        if odgovor.status_code != 200:
+            print("napaka", id)
+            continue
+
+        with open(os.path.join("knjige", f"knjige{id}.html"), "w", encoding="utf-8") as dat:
+            dat.write(odgovor.text)
+        time.sleep(1)
